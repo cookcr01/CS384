949098559b31b0943006b407e0d0b33e4136ad67
cookcr01 <cameroncook6@yahoo.com>
2018-02-18 00:16:36 +0000

    changed some stuff

--- HW4/NewtonTri.java
+++ HW4/NewtonTri.java
@@ -2,19 +2,19 @@ public class NewtonTri{
     public static void main(String[] args) {
         double [] x = {0.0,0.1,0.3,0.6,1.0,1.1};
         double [] fx = {-6.00000, -5.89483,-5.65014, -5.17788,-4.28172,-3.99583};
-        double [] result = dd(x,fx);
+        dd(x,fx);
     }
     
     public static void dd (double [] x,  double [] fx){
         int n = x.length;
-        double[] dividedDifferences = fx.clone();
+        double[] result = fx.clone();
         for(int i=1; i<n; i++) {
             System.out.println("Step "+i+": ");
             System.out.println("---------------");
             for(int j=n-1; j>0; j--) {
                 if(j-i>=0) {
-                    dividedDifferences[j] = (dividedDifferences[j]-dividedDifferences[j-1])/(x[j]-x[j-i]);
-                    System.out.println(dividedDifferences[j]);
+                    result[j] = (result[j]-result[j-1])/(x[j]-x[j-i]);
+                    System.out.println(result[j]);
                 }
             }
             System.out.println("");
