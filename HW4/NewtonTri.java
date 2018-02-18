82d78106b4aed0cfa8fece31fe999f73392780af
cookcr01 <cameroncook6@yahoo.com>
2018-02-18 00:08:02 +0000

    newton triangle

--- HW4/NewtonTri.java
+++ HW4/NewtonTri.java
@@ -1,10 +1,23 @@
 public class NewtonTri{
     public static void main(String[] args) {
-        double [] x = {0.0,0.1,0.3,0.6,1.0};
-        double [] fx = {-6.00000,-5.89483,-5.65014,-5.17788,-4.28172};
+        double [] x = {0.0,0.1,0.3,0.6,1.0,1.1};
+        double [] fx = {-6.00000, -5.89483,-5.65014, -5.17788,-4.28172,-3.99583};
+        double [] result = dd(x,fx);
     }
     
-    public static dividedDifferences(){
-        
+    public static void dd (double [] x,  double [] fx){
+        int n = x.length;
+        double[] dividedDifferences = fx.clone();
+        for(int i=1; i<n; i++) {
+            System.out.println("Step "+i+": ");
+            System.out.println("---------------");
+            for(int j=n-1; j>0; j--) {
+                if(j-i>=0) {
+                    dividedDifferences[j] = (dividedDifferences[j]-dividedDifferences[j-1])/(x[j]-x[j-i]);
+                    System.out.println(dividedDifferences[j]);
+                }
+            }
+            System.out.println("");
+        }   
     }
 }
