3fc1089a38e0a8c250e87995abc1002689187472
cookcr01 <cameroncook6@yahoo.com>
2018-02-18 00:40:19 +0000

    good enough for me

--- HW4/NewtonTri.java
+++ HW4/NewtonTri.java
@@ -2,22 +2,35 @@ public class NewtonTri{
     public static void main(String[] args) {
         double [] x = {0.0,0.1,0.3,0.6,1.0,1.1};
         double [] fx = {-6.00000, -5.89483,-5.65014, -5.17788,-4.28172,-3.99583};
-        dd(x,fx);
+        double[] result = dd(x,fx);
+        
+        //printTri(result);
     }
+    // public static void printTri(double[] result){
+    //         for(int i = 0; i<result.length; i++){
+    //             if(result[i]!= 0){
+    //             System.out.println(result[i]);
+    //         }
+    //     }
+    // }
     
-    public static void dd (double [] x,  double [] fx){
+    public static double[] dd (double [] x,  double [] fx){
         int n = x.length;
+        int index = 0;
+        double[] a = new double[100];
         double[] result = fx.clone();
         for(int i=1; i<n; i++) {
-            System.out.println("Step "+i+": ");
-            System.out.println("---------------");
+            System.out.println("Step "+ i+": ");
             for(int j=n-1; j>0; j--) {
                 if(j-i>=0) {
                     result[j] = (result[j]-result[j-1])/(x[j]-x[j-i]);
+                    a[index]= result[j];
+                    index++;
                     System.out.println(result[j]);
                 }
             }
             System.out.println("");
-        }   
+        }
+        return a;
     }
 }
